ac428d6d913ec1c2bf96171832d952d95fd075fb
MeryAmun <chebesiawah@gmail.com>
2022-01-17 08:22:03 -0800

    base structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { fetchWeather } from './api/fetchWeather'
+import '.App.css'
+
+export const App = () => {
+    return (
+        <div>
+            pwa
+        </div>
+    )
+}
